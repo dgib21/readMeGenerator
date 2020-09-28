ebacbc635d14184ff25f8da4a9dc13e93ef0271d
Devin Gibson <dgib21@hotmail.com>
2020-09-27 23:28:06 -0500

    fixing markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,39 +3,30 @@ function generateMarkdown(userResponse) {
   console.log(userResponse);
   
     return `
-    # ${userResponse.title}\n
-    
-    ## Description:\n
+# ${userResponse.title}\n
+## Description:\n
       ${userResponse.description}\n
-
-    # Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    - [Contribution](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-
-    ## Installation:\n
+# Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contribution](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+## Installation:\n
     ${userResponse.installation}\n
-
-    ## Usage:\n
+## Usage:\n
     ${userResponse.usage}\n
-
-    ## Credits:\n
+## Credits:\n
     ${userResponse.credits}\n
-
-    ## License:\n
+## License:\n
     For more information about licenses, please visit:
     [License](https://opensource.org/licenses/${userResponse.license})\n
-
-    ## Contributing:\n    
+## Contributing:\n    
     ${userResponse.contributing}\n
-    
-    ## Tests:\n
+## Tests:\n
     ${userResponse.tests}\n
-    
     ## Questions:\n
     For questions or concerns please use the following: 
     - [Github Profile](https://github.com/${userResponse.github})
